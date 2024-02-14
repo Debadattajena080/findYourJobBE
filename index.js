ce2de87d9fa66b6b0bba3f7292349836f1b07e6e
Debadattajena080 <110017769+Debadattajena080@users.noreply.github.com>
2024-02-14 12:35:56 +0530

    Update  local host index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const jobRouters = require("./routes/jobRoutes");
 
 app.use(cors());
 const PORT = 5000;
-const DB_URL = "mongodb://127.0.0.1:27017/JobSeekers";
+const DB_URL = "mongodb://localhost:27017/JobSeekers";
 
 app.use(express.json());
 

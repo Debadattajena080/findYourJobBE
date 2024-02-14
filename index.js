c2fee27188ebded9c5c267e63b09d745c61e0657
Debadattajena080 <110017769+Debadattajena080@users.noreply.github.com>
2024-02-14 12:43:04 +0530

    change in index.js

--- index.js
+++ index.js
@@ -2,32 +2,27 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
-const jobRouters = require("./routes/jobRoutes");
+// const userRoutes = require("./routes/userRoutes");
+const jobRouters = require("./routes/jobRoutes")
 
 app.use(cors());
 const PORT = 5000;
-const DB_URL = "mongodb://localhost:27017/JobSeekers";
-
+DB_URL = "mongodb://127.0.0.1:27017/JobSeekers";
 app.use(express.json());
-
-const connectToDatabase = async () => {
-  try {
-    await mongoose.connect(DB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      // useFindAndModify: false,
-      // useCreateIndex: true,
+mongoose
+  .connect(DB_URL,{
+    family: 4,
+  })
+  .then(() => {
+    console.log("Db is connected successfully");
+    app.listen(PORT, () => {
+      console.log(`Server started on ${PORT}`);
     });
-    console.log("MongoDB connected successfully");
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-  }
-};
-
-connectToDatabase().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server started on ${PORT}`);
+  })
+  .catch((error) => {
+    console.log("error", error);
   });
-});
+
+ 
 
 app.use(jobRouters);
